78d9a7d7694528a53b23014d87f39d31d21b444e
user <aiselcms.aj@gmail.com>
2023-07-05 10:46:45 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { default as create } from './create';
-export { default as verify } from './verify';
+export { default as create } from './create/index';
+export { default as verify } from './verify/index';
